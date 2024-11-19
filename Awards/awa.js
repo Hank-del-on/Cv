c5c6f9a239ccaa9b24d9551e02ee0b510e99db8f
Hank-del-on <hankhankkodehode@gmail.com>
2024-11-19 13:34:56 +0100

    Update awa.js

--- Awards/awa.js
+++ Awards/awa.js
@@ -6,6 +6,18 @@ function draw() {
     ctx.fillRect(10, 10, 50, 50);
     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
     ctx.fillRect(30, 30, 50, 50);
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(70, 30, 50, 50);
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(90, 30, 50, 50);
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(120, 30, 50, 50);
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(170, 50, 50, 50);
+    ctx.fillStyle = "rgba(0, 0, 0, 43.5)";
+    ctx.fillRect(190, 60, 120, 190);
+    ctx.fillStyle = "rgba(0, 0, 10, 1.5)";
+    ctx.fillRect(0, 60, 190, 190);
   }
 }
 
@@ -16,10 +28,16 @@ $(document).ready(function () {
       opacity: 5,
       marginTop: "0",
     },
-    8300
+    20000
   );
   $("h2").click(function () {
     $(this).next(".subtext").slideToggle("fast");
     $(this).children(".hex").toggleClass("moved");
   });
 });
+
+// if (confirm("Press a button!")) {
+//   txt = "You pressed OK!";
+// } else {
+//   txt = "You pressed Cancel!";
+// }
